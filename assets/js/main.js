2454d12e99fa5ffd2a0819603f021863d2837968
Farzaneh Taheri <143893864+farzaneh-taheri@users.noreply.github.com>
2025-12-14 11:39:22 +0330

    feat: add responsive hamburger menu logic and import home.css in main.css and import header.js in main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,5 @@
-//منوی همبرگری با دکمه بستن
-const hamburger = document.getElementById("hamburger");
-const navLinks = document.getElementById("nav-links");
-const closeMenu = document.getElementById("close-menu");
+import { initHeaderMenu } from "./modules/header.js";
 
-hamburger.addEventListener("click", () => {
-  navLinks.classList.add("active");
-});
-
-closeMenu.addEventListener("click", () => {
-  navLinks.classList.remove("active");
+document.addEventListener("DOMContentLoaded", () => {
+  initHeaderMenu();
 });
